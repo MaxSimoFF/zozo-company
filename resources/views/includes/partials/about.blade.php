bb546bf188e361d87ef21e379490d18222b6030c
MaxSimoFF <moustafaadel019@gmail.com>
2022-11-04 17:16:22 +0200

    fix height of details on services and fix some spelling words

---
+++ resources/views/includes/partials/about.blade.php
@@ -0,0 +1,69 @@
+<section id="about" class="about">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+            <h2>O nas</h2>
+            <p>Kancelaria Liczydło to marka, która jest obecna na polskim rynku już od ponad dekady. Codziennie pomagamy
+                i wspieramy zarówno dużych przedsiębiorców jak i rodzinne firmy, fundacje, stowarzyszenia. Sami
+                zaczynaliśmy lata temu jako mała rodzinna firma zajmująca się usługami księgowymi, na bazie której
+                zbudowaliśmy jedno z największych biur w regionie. Dzisiaj możemy pochwalić się zespołem złożonym z
+                profesjonalistów w takich dziedzinach jak rachunkowość, kadry, płace i podatki. Stale podnosimy swoje
+                kwalifikacje w dbałości o jakość naszych usług. Słuchamy potrzeb naszych klientów i szukamy możliwie
+                najlepszych rozwiązań, aby spełnić ich oczekiwania. Naszą wizytówką są partnerskie relacje z klientami i
+                ich referencje.</p>
+        </div>
+
+        <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
+
+            <div class="col-lg-5">
+                <div class="about-img">
+                    <img src="assets/img/about.webp" class="img-fluid" alt="">
+                </div>
+            </div>
+
+            <div class="col-lg-7">
+                <!-- Tabs -->
+                <ul class="nav nav-pills mb-3">
+                    <li><a class="nav-link active" data-bs-toggle="pill" href="#tab1">o nas</a></li>
+                </ul><!-- End Tabs -->
+
+                <!-- Tab Content -->
+                <div class="tab-content">
+
+                    <div class="tab-pane fade show active" id="tab1" style="line-height: 1.8;">
+                        <div class="d-flex align-items-center mt-4">
+                            <i class="bi bi-check2"></i>
+                            <h4>Jesteśmy ambitnym Zespołem!</h4>
+                        </div>
+                        <p>Jesteśmy ambitnym Zespołem! Stale dbamy o podnoszenia naszych kwalifikacji aby odnosić
+                            sukcesy wspólnie z naszymi partnerami. Dbamy o wysoki standart i bezpieczeństwo naszych
+                            usług inwestując w nowe technologie i najlepsze programy dostępne na rynku. Z nasi
+                            kontrahenci mają zapewnione maximum bezpieczeńswa przy możliwe najwyższej jakości usług
+                            dostępnych na rynku.</p>
+
+                        <div class="d-flex align-items-center mt-4">
+                            <i class="bi bi-check2"></i>
+                            <h4>Słuchamy potrzeb naszych klientów</h4>
+                        </div>
+                        <p>Zwracamy uwagę na zdanie naszych klientów co pozwala nam wychodzić na przeciw ich
+                            oczekiwaniom. Stale szukamy nowych rozwiązań na dynamicznym rynku aby dać naszym
+                            kontrahentom jak najlepszy grunt do rozwoju ich działalności.</p>
+
+                        <div class="d-flex align-items-center mt-4">
+                            <i class="bi bi-check2"></i>
+                            <h4>Z dumą możemy pochwalić się relacjami z Klientami</h4>
+                        </div>
+                        <p>W naszej codziennej pracy pomagamy firmom rodzinnym, fundacjom i stowarzyszeniom, jak również
+                            podmiotom sklasyfikowanym jako duże przedsiębiorstwa. Naszą wizytówką są partnerskie relacje
+                            z klientami i ich referencje.</p>
+
+
+                    </div><!-- End Tab 1 Content -->
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</section>
---
+++ resources/views/includes/partials/services.blade.php
@@ -0,0 +1,126 @@
+<section id="services" class="services">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+            <h2>Nasze usługi</h2>
+            <p>Stale szkolimy się w każdej dziedzinie którą obejmuje nasza działalność aby świadczyć usługi na
+                najwyższym poziomie.</p>
+        </div>
+
+        <div class="row gy-5">
+
+            <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
+                <div class="service-item">
+                    <div class="img">
+                        <img src="assets/img/services-1.webp" class="img-fluid" alt="">
+                    </div>
+                    <div class="details position-relative">
+                        <div class="icon">
+                            <i class="bi bi-person-rolodex"></i>
+                        </div>
+                        <a href="#" class="stretched-link">
+                            <h3>Usługi księgowe</h3>
+                        </a>
+                        <p>Kompleksowe usługi księgowe, ryczałt, PKPiR, Księgi handlowe.</p>
+                    </div>
+                </div>
+            </div><!-- End Service Item -->
+
+            <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="300">
+                <div class="service-item">
+                    <div class="img">
+                        <img src="assets/img/services-2.webp" class="img-fluid" alt="">
+                    </div>
+                    <div class="details position-relative">
+                        <div class="icon">
+                            <i class="bi bi-wallet2"></i>
+                        </div>
+                        <a href="#" class="stretched-link">
+                            <h3>Usługi kadrowo - płacowe</h3>
+                        </a>
+                        <p>Prowadzenie teczek osobowych, naliczanie wynagrodzeń, Rozliczenia z ZUS oraz urzędem
+                            skarbowym w zakresie umów o pracę, umów cywilnoprawnych.</p>
+                    </div>
+                </div>
+            </div><!-- End Service Item -->
+
+            <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="400">
+                <div class="service-item">
+                    <div class="img">
+                        <img src="assets/img/services-3.webp" class="img-fluid" alt="">
+                    </div>
+                    <div class="details position-relative">
+                        <div class="icon">
+                            <i class="bi bi-bank"></i>
+                        </div>
+                        <a href="#" class="stretched-link">
+                            <h3>Doradztwo podatkowe</h3>
+                        </a>
+                        <p>Audyty podatkowe, Wnioski interpretacyjne. Reprezentacja kontrahentów przed organami
+                            administracji skarbowej, Doradztwo w zakresie PIT, CIT, VAT.</p>
+                    </div>
+                </div>
+            </div><!-- End Service Item -->
+
+            <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="500">
+                <div class="service-item">
+                    <div class="img">
+                        <img src="assets/img/services-4.webp" class="img-fluid" alt="">
+                    </div>
+                    <div class="details position-relative">
+                        <div class="icon">
+                            <i class="bi bi-check-all"></i>
+                        </div>
+                        <a href="#" class="stretched-link">
+                            <h3>Nadzór księgowy</h3>
+                        </a>
+                        <p>Nadzór nad prowadzeniem ewidencji księgowej i podatkowej. Sporządzanie polityki rachunkowości
+                            oraz zakładowego planu kont. Audyty ksiąg rachunkowych i podatkowych.</p>
+                        <a href="#" class="stretched-link"></a>
+                    </div>
+                </div>
+            </div><!-- End Service Item -->
+
+            <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="600">
+                <div class="service-item">
+                    <div class="img">
+                        <img src="assets/img/services-5.webp" class="img-fluid" alt="">
+                    </div>
+                    <div class="details position-relative">
+                        <div class="icon">
+                            <i class="bi bi-intersect"></i>
+                        </div>
+                        <a href="#" class="stretched-link">
+                            <h3>Szkolenia branżowe</h3>
+                        </a>
+                        <p>W zakresie bieżących zmian podatkowych PIT, CIT, VAT. Szkolenia dotyczących księgowości, kadr
+                            i płac.</p>
+                        <a href="#" class="stretched-link"></a>
+                    </div>
+                </div>
+            </div><!-- End Service Item -->
+
+            <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="700">
+                <div class="service-item">
+                    <div class="img">
+                        <img src="assets/img/services-6.webp" class="img-fluid" alt="">
+                    </div>
+                    <div class="details position-relative">
+                        <div class="icon">
+                            <i class="bi bi-graph-up-arrow"></i>
+                        </div>
+                        <a href="#" class="stretched-link">
+                            <h3>Doradztwo biznesowe</h3>
+                        </a>
+                        <p>Doradztwo w zakresie wyboru formy opodatkowania, zakładania jednoosobowej działalności
+                            gospodarczej, zakładania spółek prawa handlowego, pozyskiwania dofinansowanie zewnętrznego.
+                        </p>
+                        <a href="#" class="stretched-link"></a>
+                    </div>
+                </div>
+            </div><!-- End Service Item -->
+
+        </div>
+
+    </div>
+</section>
